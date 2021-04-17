20faef8d5745833688444d6449be0974009c98ce
Wink Saville <wink@saville.com>
2021-04-17 16:26:26 -0700

    Add struct Symbol and create a Vec of Symbols

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+pub struct Symbol {
+    pub name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut symbols = Vec::<Symbol>::new();
+
+    symbols.append(&mut vec![
+        Symbol {
+            name: "BTCUSD".to_string(),
+        },
+        Symbol {
+            name: "BNBUSD".to_string(),
+        },
+    ]);
+    println!("symbols={:#?}", symbols);
 }
