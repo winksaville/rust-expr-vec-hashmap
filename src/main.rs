050469e2b12b7173ca24ab7b82a0719991ec1e26
Wink Saville <wink@saville.com>
2021-04-17 16:33:51 -0700

    Add creating HashMap<&str, &Symbol>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct Symbol {
     pub name: String,
@@ -15,4 +17,9 @@ fn main() {
         },
     ]);
     println!("symbols={:#?}", symbols);
+
+    let mut symbols_hm = HashMap::<&str, &Symbol>::new();
+    symbols_hm.insert(&symbols[0].name, &symbols[0]);
+    symbols_hm.insert(&symbols[1].name, &symbols[1]);
+    println!("symbols_hm={:#?}", symbols_hm);
 }
