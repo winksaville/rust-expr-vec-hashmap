20de3e26d6d6f03715c6414d5dd8da45dd77c9ee
Wink Saville <wink@saville.com>
2021-04-19 15:02:00 -0700

    Add symbol associated functions to ExchangeInfo
     Add create_symbol_hash_map, get_symbol_via_hash_map
    
    Fn get_symbol is an attempt to "lazily" initialize symbols_hm, but it
    only works ONCE. Subsequent attempts cause a compile error. At this
    point I see this as a good thing because it clearly forbids improper
    use of ExchangeInfo!

--- src/main.rs
+++ src/main.rs
@@ -22,22 +22,24 @@ pub fn create_hash_map(symbols: &[Symbol]) -> HashMap<&str, &Symbol> {
 }
 
 #[derive(Debug)]
-pub struct ExchangeInfo {
+pub struct ExchangeInfo<'e> {
     pub server_time: i64,
     pub symbols: Vec<Symbol>,
+    symbols_hm: HashMap<&'e str, &'e Symbol>,
 }
 
-impl Default for ExchangeInfo {
-    fn default() -> ExchangeInfo {
+impl<'e> Default for ExchangeInfo<'e> {
+    fn default() -> ExchangeInfo<'e> {
         ExchangeInfo {
             server_time: 0,
             symbols: vec![],
+            symbols_hm: HashMap::<&str, &Symbol>::new(),
         }
     }
 }
 
-impl ExchangeInfo {
-    pub fn new() -> ExchangeInfo {
+impl<'e> ExchangeInfo<'e> {
+    pub fn new() -> ExchangeInfo<'e> {
         let mut ei = ExchangeInfo::default();
         ei.update_server_time();
         ei
@@ -53,26 +55,63 @@ impl ExchangeInfo {
         self
     }
 
-    pub fn get_symbol(&self, name: &str) -> Option<&Symbol> {
-        for sym in &self.symbols {
-            if name == sym.name {
-                return Some(sym);
-            }
+    fn create_symbol_hash_map(&'e mut self) -> &Self {
+        self.symbols_hm = create_hash_map(&self.symbols);
+        self
+    }
+
+    fn get_symbol_via_hm(&self, name: &str) -> Option<&Symbol> {
+        Some(*self.symbols_hm.get(name)?)
+    }
+
+    pub fn get_symbol(&'e mut self, name: &'e str) -> Option<&'e Symbol> {
+        let sym = self.symbols_hm.get(name);
+        if sym.is_none() && self.symbols_hm.is_empty() {
+            Some(*self.create_symbol_hash_map().symbols_hm.get(name)?)
+        } else {
+            Some(*sym?)
         }
-        None
     }
 }
 
 fn main() {
-    let mut ei = ExchangeInfo::new();
-    println!("ei={:#?}", ei);
-    ei.add_symbol(Symbol::new("BTCUSD".to_string()));
-    ei.add_symbol(Symbol::new("BNBUSD".to_string()));
-    println!("ei={:#?}", ei);
+    let mut mut_ei = ExchangeInfo::new();
+    println!("mut_ei={:#?}", mut_ei);
+    mut_ei.add_symbol(Symbol::new("BTCUSD".to_string()));
+    mut_ei.add_symbol(Symbol::new("BNBUSD".to_string()));
+    println!("mut_ei={:#?}", mut_ei);
 
-    //let symbols_hm = create_hash_map(&ei.symbols);
-    //println!("symbols_hm={:#?}", symbols_hm);
-    println!("BTCUSD={:#?}", ei.get_symbol("BTCUSD"));
-    println!("BNBUSD={:#?}", ei.get_symbol("BNBUSD"));
-    println!("XYZUSD={:#?}", ei.get_symbol("XYZUSD"));
+    if true {
+        // OK any number of times
+        let ei = mut_ei.create_symbol_hash_map();
+        println!("BTCUSD={:#?}", ei.get_symbol_via_hm("BTCUSD"));
+        println!("BNBUSD={:#?}", ei.get_symbol_via_hm("BNBUSD"));
+        println!("XYZUSD={:#?}", ei.get_symbol_via_hm("XYZUSD"));
+    } else {
+        // OK once
+        println!("BNBUSD={:#?}", mut_ei.get_symbol("BNBUSD"));
+
+        //   Below fails with compile error if used again! This is "less" convenient,
+        //   but it makes things obvious that there is an initialization phase where
+        //   `mut_ei` is mutable and not "fnished" and when it's complete and now can
+        //   be referenced multiple times.
+        // println!("BNBUSD={:#?}", mut_ei.get_symbol("BNBUSD"));
+        //
+        //   Here is the compile error for above:
+        //    $ cargo run
+        //       Compiling expr-vec-hashmap v0.1.0 (/home/wink/prgs/rust/projects/expr-vec-hashmap)
+        //    error[E0499]: cannot borrow `mut_ei` as mutable more than once at a time
+        //      --> src/main.rs:98:34
+        //       |
+        //    92 |         println!("BNBUSD={:#?}", mut_ei.get_symbol("BNBUSD"));
+        //       |                                  ------ first mutable borrow occurs here
+        //    ...
+        //    98 |         println!("BNBUSD={:#?}", mut_ei.get_symbol("BNBUSD"));
+        //       |                                  ^^^^^^
+        //       |                                  |
+        //       |                                  second mutable borrow occurs here
+        //       |                                  first borrow later used here
+        //
+        //    error: aborting due to previous error
+    }
 }
